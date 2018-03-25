45cf177d297b7e2e8c2927a487b1a36f8a247f0b
wessamKoraim <wessamadel1996@gmail.com>
2018-03-26 00:21:00 +0200

    new task

--- Arcade/palindromeRearranging.java
+++ Arcade/palindromeRearranging.java
@@ -9,14 +9,12 @@ boolean palindromeRearranging(String inputString)
     int sizeOfInputString = inputString.length();
     
     for(int i = 0; i < sizeOfInputString; i++)
-        letterCount[inputStringCopy[i] - 'a']++;
+        letterCount[inputStringCopy[i] - 'a'] += 1;
 
     for(int i = 0; i < 26; i++)
-    {
         if( ((letterCount[i] % 2) != 0) && (!oddOccured) )
             oddOccured = true;
         else if( ((letterCount[i] % 2) != 0) && (oddOccured) )
             return false;
-    }
     return true;
 }
